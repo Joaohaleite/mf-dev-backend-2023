0c0d85e5973f7a17feb387fdadb41fe8a1a6d91c
joaohleite <jhaleite@sga.pucminas.br>
2024-10-25 16:57:20 -0300

    Atualização

---
+++ mf-dev-backend-2023/Models/Consumo.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage ="Obrigatório infiormar descrição!")]
+        [Display(Name ="Descrição")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório infiormar data!")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório infiormar Valor!")]
+        public decimal Valor { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório infiormar Km!")]
+        public int Km { get; set; }
+
+        [Display(Name = "Tipo de Combustível")]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Display(Name = "Veículo")]
+        [Required(ErrorMessage = "Obrigatório infiormar o Veículo!")]
+        public int VeiculoId { get; set; }
+
+        [ForeignKey("VeiculoId")]
+        public Veiculo Veiculo { get; set; }
+    }
+
+    public enum TipoCombustivel 
+    {
+        Gasolina,
+        Etanol
+    }
+}
---
+++ mf-dev-backend-2023/Models/DataTime.cs
@@ -0,0 +1,6 @@
+﻿namespace mf_dev_backend_2023.Models
+{
+    public class DataTime
+    {
+    }
+}
